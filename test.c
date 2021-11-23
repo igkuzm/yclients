6d2c21a496f9f6901436ebf97d7c1e3e9cd962db
Igor Sementsov <ig.kuzm@gmail.com>
2021-11-23 22:20:45 +1000

    Updated test.c

--- test.c
+++ test.c
@@ -10,20 +10,28 @@
 #include "yclients.h"
 
 
-int callback(int count, void *ptr, bool hasFinished){
-	struct yclients_client_t *clients = ptr;
-	printf("LOADED %d CLIENTS\n", count);
+//int callback(int count, void *ptr, bool hasFinished){
+	//struct yclients_client_t *clients = ptr;
+	//printf("LOADED %d CLIENTS\n", count);
+	//int i;
+	//for (i = 0; i < count; ++i) {
+		//printf("CLIENT ID: %d\tNAME: %s\n", clients[i].id, clients[i].name);
+	//}
+	//if (hasFinished) {
+		//printf("FINISHED\n");
+	//}
+	//return 0;
+//}
+
+//6672348
+
+int callback(int error, int count, void *context, struct yclients_client_t *clients){
 	int i;
 	for (i = 0; i < count; ++i) {
-		printf("CLIENT ID: %d\tNAME: %s\n", clients[i].id, clients[i].name);
-	}
-	if (hasFinished) {
-		printf("FINISHED\n");
+		printf("NAME: %s\n", clients[i].name);
 	}
 	return 0;
 }
-
-//6672348
 
 int main(int argc, char *argv[])
 {
@@ -42,13 +50,13 @@ int main(int argc, char *argv[])
 
 	//ID 120372693
 	//
-	struct yclients_event_t *events;
-	int count = yclients_get_events_for_client(&events, "xr693jct2mdw7e9dzzge", "79147341055", "puewj3", 69643, 35390745);	
-	int i;
-	for (i = 0; i < count; ++i) {
-		struct yclients_client_t client = events[i].client;
-		printf("EVENT ID: %d, DATE: %s, CLIENT: %s\n", events[i].id, events[i].date, client.name);
-	}
+	//struct yclients_event_t *events;
+	//int count = yclients_get_events_for_client(&events, "xr693jct2mdw7e9dzzge", "79147341055", "puewj3", 69643, 35390745);	
+	//int i;
+	//for (i = 0; i < count; ++i) {
+		//struct yclients_client_t client = events[i].client;
+		//printf("EVENT ID: %d, DATE: %s, CLIENT: %s\n", events[i].id, events[i].date, client.name);
+	//}
 	//
 	//struct yclients_client_t *clients;
 	//int count = yclients_get_clients_with_search_pattern(&clients, "xr693jct2mdw7e9dzzge", "79147341055", "puewj3", 69643, "89990407731");
@@ -58,6 +66,10 @@ int main(int argc, char *argv[])
 		//printf("ID: %d, NAME: %s, PHONE: %s\n", clients[i].id, clients[i].name, clients[i].phone);
 		
 	//}
+	
+	yclients_get_clients_names_with_search("xr693jct2mdw7e9dzzge", "79147341055", "puewj3", 69643, "89990407731", NULL, callback);
+	
+	getchar();
 	
 	/*struct yclients_client_t *clients;*/
 	/*char *fields[] = {"id", "name", "phone"};*/
