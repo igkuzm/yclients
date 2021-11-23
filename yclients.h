82ef83e82ac30560aae71aeb7b4e30cfb9167e42
Igor Sementsov <ig.kuzm@gmail.com>
2021-11-23 21:21:11 +1000

    Updated yclients.h

--- yclients.h
+++ yclients.h
@@ -154,10 +154,10 @@ int yclients_get_clients_names_with_search
 				const char *password,				  //password 
 				long companyId,						  //ID of company 
 				const char *searchString			  //search string - eg. telephone number or name
-				void context,
+				void context,						  //context to pass trow callback
 				int (*callback)(                      //callback - return non 0 to stop function
 					struct yclients_client_t *client, //pointer of client (struct yclients_client_t)
-					void context				      //true if has got all records
+					void context				      //context callback return
 				)
 				
 );
